32a9274cf5885df7170963bcb094625aa54be7a6
Alexander <sasstr@gmail.com>
2018-03-06 00:56:26 +0300

    Нарезал графику с макетов и подключил картинки
    Экспортировал графику из макетов и оптимизировал её

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var slider = document.querySelectorAll( 'slider-item' );
+var radioButtons = document.querySelectorAll( 'slider input[type="radio"]' );
+
+console.log(radioButtons);
+console.log(slider);
