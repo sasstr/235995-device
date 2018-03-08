e791e014fa4e0d1834a6981258229de2c7753c5f
Alexander <sasstr@gmail.com>
2018-03-08 17:02:35 +0300

    Нписал скрипт для слайдера и блока сервисов
    Нписал скрипт для слайдера и блока сервисов

--- js/main.js
+++ js/main.js
@@ -1,5 +1,50 @@
-var slider = document.querySelectorAll( 'slider-item' );
-var radioButtons = document.querySelectorAll( 'slider input[type="radio"]' );
+var sliders = document.querySelectorAll( '.slider-item' );
+var radioButtonsSlider = document.querySelectorAll( '.slider input[type="radio"]' );
+var formRadioButtonsSlider = document.querySelector( '.slider form' );
 
+var servises = document.querySelectorAll( '.services-data-feature' );
+var radioButtonsServises = document.querySelectorAll( '.service-data-controls input[type="radio"]' );
+var formRadioButtonsServises = document.querySelector( '.service-data-controls form' );
+
+formRadioButtonsSlider.onclick = function(){
+  for (var i = 0; i < sliders.length ; i++) {
+
+  if ( radioButtonsSlider[i].checked == true ) {
+          sliders[i].classList.remove('visually-hidden');
+    }
+  else if ( radioButtonsSlider[i].checked == false ){
+          sliders[i].classList.add('visually-hidden');
+    }
+  }
+}
+
+window.onload = function () {
+
+  var len = sliders.length,
+      i = len-1;
+  (function go() {
+    sliders[i].classList.add('visually-hidden');
+     i = ++i % len;
+     sliders[i].classList.remove('visually-hidden');
+
+     window.setTimeout(go, 3000);
+    })();
+  }
+}
+
+formRadioButtonsServises.onclick = function(){
+  for (var i = 0; i < sliders.length ; i++) {
+
+  if ( radioButtonsServises[i].checked == true ) {
+    servises[i].classList.remove('visually-hidden');
+      }
+  else if ( radioButtonsServises[i].checked == false ){
+    servises[i].classList.add('visually-hidden');
+      }
+    }
+  }
+
+
+/*  console.log("ok-1!");
 console.log(radioButtons);
-console.log(slider);
+     */
